4ed2dd478aebb45e6a089504948e72b1be76b7cd
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-18 16:35:19 -0300

    novo portifolio

--- script.js
+++ script.js
@@ -1,55 +1,9 @@
-
-const summary = () => {
-    const aboutme = document.getElementById('about-me');
-    aboutme.addEventListener('click', () => {
-        window.location.href = '#about-me-here';
-    });
-    const projects = document.getElementById('projetos');
-    projects.addEventListener('click', () => {
-        window.location.href = '#projetos-here';
-    });
-    const contact = document.getElementById('contato');
-    contact.addEventListener('click', () => {
-        window.location.href = '#contato-here';
-    });
-}
-const linkProjects = () => {
-    const pixelsArt = document.querySelector('.pixels-art');
-    const shoppingCart = document.querySelector('.shopping-cart');
-    const todolist = document.querySelector('.project-todolist');
-    const memeGenerator = document.querySelector('.meme-generator');
-
-    pixelsArt.addEventListener('click', () => {
-        window.location.href = 'https://rodrigodesouzafernandes.github.io/pixels-art/';
-    });
-    shoppingCart.addEventListener('click', () => {
-        window.location.href = 'https://rodrigodesouzafernandes.github.io/shopping-cart-project/'
-    });
-    todolist.addEventListener('click', () => {
-        window.location.href = 'https://rodrigodesouzafernandes.github.io/project-to-do-list/'
-    });
-    memeGenerator.addEventListener('click', () => {
-        window.location.href = 'https://rodrigodesouzafernandes.github.io/project-meme-generator/'
-    });
-}
-const social = () => {
-    const linkedin = document.getElementById('linkedin');
-    const instagram = document.getElementById('instagram');
-    const github = document.getElementById('github');
-    linkedin.addEventListener('click', () => {
-        window.location.href = 'https://linkedin.com/in/rodrigo-de-souza-fernandes-23348810a'
-    });
-    instagram.addEventListener('click', () => {
-        window.location.href = 'https://www.instagram.com/rodrigo.fernandes95/'
-    });
-    github.addEventListener('click', () => {
-        window.location.href = 'https://github.com/rodrigoDeSouzaFernandes'
-    });
-}
-
-
-window.onload = 
-summary(),
-linkProjects(),
-social();
-
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass('sticky');
+        }else{
+            $('.navbar').removeClass('sticky');
+        }
+    })
+})
