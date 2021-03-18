250225dcb3bfbe65b4ec0bfd11a8d4be402e4338
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-18 19:11:57 -0300

    portifolio pagina home e about

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(()=>{
+    $(window).scroll(()=>{
         if(this.scrollY > 20){
             $('.navbar').addClass('sticky');
         }else{
             $('.navbar').removeClass('sticky');
         }
+    });
+    // toggle menu navbar script
+    $('.menu-btn').click(() => {
+        $('.navbar .menu').toggleClass('active')
+        $('.menu-btn i').toggleClass('active')
     })
-})
+});
