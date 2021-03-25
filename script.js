99071da05c20242b478c7b62d62bb7802b7b513d
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-25 13:25:17 -0300

    atualizando portifolio

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
-$(document).ready(()=>{
-    $(window).scroll(()=>{
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
-            $('.navbar').addClass('sticky');
+            $('.navbar').addClass("sticky");
         }else{
-            $('.navbar').removeClass('sticky');
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
-    // toggle menu navbar script
-    $('.menu-btn').click(() => {
-        $('.navbar .menu').toggleClass('active')
-        $('.menu-btn i').toggleClass('active')  
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Desenvolvedor", "Estudante"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
-    $('carousel').owlCarousel({
+    $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0: {
+            0:{
                 items: 1,
                 nav: false
             },
-            600: {
+            600:{
                 items: 2,
                 nav: false
             },
-            1000: {
+            1000:{
                 items: 3,
                 nav: false
             }
